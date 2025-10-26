81361b3f0c824533477f04cc117700199e278672
Andrii Kovalevskyi <kovalevski.andrei@gmail.com>
2025-10-26 10:15:21 +0100

    Upgrade to next.js 16 and add admin template

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+export default {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        search: ''
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        search: ''
+      }
+    ]
+  }
 };
-
-export default nextConfig;
